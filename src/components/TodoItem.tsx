3c8930f50509561596b8fdb78bd8ddcca234dba4
yujin <wwonyujin@gmail.com>
2022-08-05 20:42:17 +0900

    feat: ✨ todo 수정, 삭제 기능 추가

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,19 +1,57 @@
 import { useState } from 'react'
+import { useAtom } from 'jotai'
 import styled from 'styled-components'
 import Form from './Form'
 import FormButton from './FormButton'
+import { deleteTodo, ResponseData, Todo, updateTodo } from '../api/todo'
+import { todosAtom } from '../atoms/todo'
+import useTokenCheck from '../hooks/useTokenCheck'
+import { useNavigate } from 'react-router-dom'
 
 export default function TodoItem({ id, title, content }: {
   id: string,
   title: string,
   content: string,
 }) {
+  const [todos, setTodos] = useAtom(todosAtom)
   const [editMode, setEditMode] = useState(false)
   const [inputTitle, setInputTitle] = useState(title)
   const [inputContent, setInputContent] = useState(content)
-  function handleUpdateTodo() {
+  const navigate = useNavigate()
+  const isValidToken = useTokenCheck()
 
+  function tokenCheck() {
+    if (!isValidToken) {
+      alert('로그인 정보가 유효하지 않습니다. 다시 로그인해 주세요.')
+      navigate('/auth/login')
+      return
+    }
   }
+
+  function handleUpdateTodo() {
+    updateTodo(id, inputTitle, inputContent)
+      .then(res => {
+        const { data } = res as ResponseData
+        const newTodos = todos.map(todo => {
+          if (todo.id === id) {
+            return data
+          } else {
+            return todo
+          }
+        })
+        setTodos(newTodos as Todo[])
+        setEditMode(false)
+      })
+  }
+
+  function handleDeleteTodo() {
+    deleteTodo(id)
+      .then(() => {
+        const newTodos = todos.filter(todo => todo.id !== id)
+        setTodos(newTodos as Todo[])
+      })
+  }
+
   return (
     <Li>
       {editMode ? (
@@ -36,14 +74,21 @@ export default function TodoItem({ id, title, content }: {
             />
           </label>
           <FormButton value="수정" disabled={false} />
+          <Button type="button" onClick={() => setEditMode(false)}>취소</Button>
         </Form>
       ) : (
         <>
           <p>{title}</p>
           <span>{content}</span>
           <div>
-            <Button onClick={() => setEditMode(true)}>수정</Button>
-            <Button>삭제</Button>
+            <Button onClick={() => {
+              tokenCheck()
+              setEditMode(true)
+            }}>수정</Button>
+            <Button onClick={() => {
+              tokenCheck()
+              handleDeleteTodo()
+            }}>삭제</Button>
           </div>
         </>
       )}
